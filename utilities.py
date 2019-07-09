c7a5f852e4b623bb09eb9341031d9f468abaa02a
Raghav Subramanian <raghavsubramanian@Raghavs-MacBook-Pro.local>
2019-07-09 01:10:41 -0700

    Static prediction added

--- utilities.py
+++ utilities.py
@@ -61,25 +61,52 @@ class Statistics:
     """
     Track Branch Prediction Statistics
     """
-    def __init__(self,stream_list):
+    def __init__(self,stream_list,types):
         self.branches = 0
-        self.mispred = 0
         self.pc = 0
         self.outcome = 0
         self.file_number=0
         self.stream_list = stream_list
+        self.type = types
     
+    def make_prediction(self,pc_val,outcome_val):
+        if self.type[0]==Pred_types[0]:
+            if self.stype[t]=='NT':
+                return [0]* len(outcome_val)
+            elif self.stype[t]=='T':
+                return [1] * len(outcome_val)
+            else:
+                raise ValueError("Invalid Type")
+        else:
+            print("NOT ACCRURAYE")
+
     def update_stats(self):
         self.branches+=1
+        store_accuracy=dict()
         for files in self.stream_list:
+            pc_list=[]
+            outcome_list=[]
             for line in bz2.BZ2File(files,"r"):
                 pc = str(line.split()[0]).replace("'","").replace("b","")
                 outcome = str(line.split()[1]).replace("'","").replace("b","")
                 self.pc =str2hex(pc)
                 self.outcome=int(outcome,16)
                 self.branches+=1
+                pc_list.append(self.pc)
+                outcome_list.append(self.outcome)
+            prediction=make_prediction(pc_list,outcome_list)
+            store_accuracy[files]=calculate_accuracy(prediction,outcome_list)
             self.file_number+=1
+        assert self.file_number==len(self.stream_list), "Some files could not be processed"
+        return store_accuracy
 
+    def calculate_accuracy(a,b):
+        assert len(a)==len(b), "Outcome and your predictions are of different lengths; possible overflow"
+        mispred = 0
+        for index in range(len(a)):
+            if a[index]!=b[index]:
+                mispred+=1
+        return float(mispred/len(a))
 
 
     """
@@ -101,11 +128,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-p_type', '--type', required=True, type=str, default='G-Share', help="Predictor Type")
     parser.add_argument('-t', '--traces', required=False, type=str, default='traces', help="Trace directory")
+    parser.add_argument('-s_type', '--static_type', required=False, type=str, default='NT', help="Trace directory")
     args = parser.parse_args()
    
     #Assert commands to check user arguments
     assert args.type in Pred_types,"Predictor type is not found"
-   
+    assert args.type == Pred_types[0] and args.static_type!=None, "Prediction is Static but not specified T/NT"
     #Intialize branch predictor
     Branch_Predictor(args.type,4,5,3)
     
@@ -114,4 +142,5 @@ if __name__ == "__main__":
     else:
         raise ValueError(Error_Trace)
     
-    Statistics(streams)
+    Static_pred = Statistics(streams,[args.type,args.static_type])
+    print(Static_pred.update_stats())
